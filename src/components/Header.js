92b5b6f622943872522d4ae45497ee0427002089
Marina Malyarchuk <pchupchu@yandex.ru>
2023-03-15 19:36:39 +0300

    добавлена смена кнопки хедера

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,40 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function Header({ loggedIn, isEmail }) {
   const navigate = useNavigate();
+  const location = useLocation();
+
   function signOut() {
     localStorage.removeItem("token");
     navigate("/sign-in", { replace: true });
+  }
+
+  function signUp() {
+    navigate("/sign-up", { replace: true });
   }
 
   return (
     <header className="header">
       <div className="header__logo"></div>
       <div className="header__auth">
-        <p className="header__login">{loggedIn ? isEmail : ""}</p>
-        <Link className="header__link" to="/sign-in" onClick={signOut}>
-          {loggedIn ? "Выйти" : "Войти"}
-        </Link>
+        {location.pathname === "/" && (
+          <p className="header__login">{loggedIn ? isEmail : ""}</p>
+        )}
+        {location.pathname === "/" && (
+          <Link className="header__link" to="/sign-in" onClick={signOut}>
+            Выйти
+          </Link>
+        )}
+        {location.pathname === "/sign-in" && (
+          <Link className="header__link" to="/sign-up" onClick={signUp}>
+            Регистрация
+          </Link>
+        )}
+        {location.pathname === "/sign-up" && (
+          <Link className="header__link" to="/sign-in" onClick={signOut}>
+            Войти
+          </Link>
+        )}
       </div>
     </header>
   );
