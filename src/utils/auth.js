efc592f40e5a49c2cc6a56893ce157335d5eae5b
Marina Malyarchuk <pchupchu@yandex.ru>
2023-03-17 23:41:56 +0300

    удалены комментарии

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -11,7 +11,6 @@ export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
-      //Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ password, email }),
@@ -22,7 +21,6 @@ export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
-      //Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
@@ -40,7 +38,6 @@ export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
-      //Accept: "application/json",
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
